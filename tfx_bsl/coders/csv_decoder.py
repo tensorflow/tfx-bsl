0057ced4c094d3312c7545a18d1426916642b8bd
zhuo <zhuo@google.com>
2019-10-28 10:16:21 -0700

    Added public APIs to parse CSV files and infer types.
    PiperOrigin-RevId: 277089296

---
+++ tfx_bsl/coders/csv_decoder.py
@@ -0,0 +1,234 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Decode CSV records into in-memory representation for tf data validation."""
+# TODO(b/131315065): optimize the CSV decoder.
+
+from __future__ import absolute_import
+from __future__ import division
+# Standard __future__ imports
+from __future__ import print_function
+
+import collections
+import csv
+import apache_beam as beam
+import enum
+import numpy as np
+import six
+from tfx_bsl.pyarrow_tf import tensorflow as tf
+from typing import Any, Dict, List, Text, Union
+
+from tensorflow_metadata.proto.v0 import statistics_pb2
+
+
+CSVCell = bytes
+# Text if Python3, bytes otherwise.
+CSVLine = Union[Text, bytes]
+ColumnName = Union[Text, bytes]
+
+
+class ColumnType(enum.IntEnum):
+  INT = statistics_pb2.FeatureNameStatistics.INT
+  FLOAT = statistics_pb2.FeatureNameStatistics.FLOAT
+  STRING = statistics_pb2.FeatureNameStatistics.STRING
+
+  # We need the following to hold for type inference to work.
+  assert INT < FLOAT
+  assert FLOAT < STRING
+
+
+ColumnInfo = collections.namedtuple(
+    "ColumnInfo",
+    [
+        "name",  # type: ColumnName
+        "type",  # type: Optional[ColumnType]
+    ])
+
+
+@beam.typehints.with_input_types(CSVLine)
+@beam.typehints.with_output_types(beam.typehints.List[CSVCell])
+class ParseCSVLine(beam.DoFn):
+  """A beam.DoFn to parse CSVLines into List[CSVCell]."""
+
+  __slots__ = ["_delimiter", "_reader"]
+
+  def __init__(self, delimiter: Union[Text, bytes]):
+    self._delimiter = delimiter
+    self._reader = None
+
+  def setup(self):
+    self._reader = _CSVRecordReader(self._delimiter)
+
+  def process(self, csv_line: CSVLine):
+    yield self._reader.read_line(csv_line)
+
+
+@beam.typehints.with_input_types(beam.typehints.List[CSVCell])
+@beam.typehints.with_output_types(beam.typehints.List[ColumnInfo])
+class ColumnTypeInferrer(beam.CombineFn):
+  """A beam.CombineFn to infer CSV Column types.
+
+  Its input can be produced by ParseCSVLine().
+  """
+
+  def __init__(self, column_names: List[ColumnName],
+               skip_blank_lines: bool) -> None:
+    """Initializes a feature type inferrer combiner."""
+    self._column_names = column_names
+    self._skip_blank_lines = skip_blank_lines
+
+  def create_accumulator(self) -> Dict[ColumnName, ColumnType]:
+    """Creates an empty accumulator to keep track of the feature types."""
+    return {}
+
+  def add_input(self, accumulator: Dict[ColumnName, ColumnType],
+                cells: List[CSVCell]) -> Dict[ColumnName, ColumnType]:
+    """Updates the feature types in the accumulator using the input row.
+
+    Args:
+      accumulator: A dict containing the already inferred feature types.
+      cells: A list containing feature values of a CSV record.
+
+    Returns:
+      A dict containing the updated feature types based on input row.
+
+    Raises:
+      ValueError: If the columns do not match the specified csv headers.
+    """
+    # If the row is empty and we don't want to skip blank lines,
+    # add an empty string to each column.
+    if not cells and not self._skip_blank_lines:
+      cells = ["" for _ in range(len(self._column_names))]
+    elif cells and len(cells) != len(self._column_names):
+      raise ValueError("Columns do not match specified csv headers: %s -> %s" %
+                       (self._column_names, cells))
+
+    # Iterate over each feature value and update the type.
+    for column_name, cell in zip(self._column_names, cells):
+
+      # Get the already inferred type of the feature.
+      previous_type = accumulator.get(column_name, None)
+      # Infer the type from the current feature value.
+      current_type = _infer_value_type(cell)
+
+      # If the type inferred from the current value is higher in the type
+      # hierarchy compared to the already inferred type, we update the type.
+      # The type hierarchy is,
+      #   INT (level 0) --> FLOAT (level 1) --> STRING (level 2)
+      if previous_type is None or current_type > previous_type:
+        accumulator[column_name] = current_type
+    return accumulator
+
+  def merge_accumulators(
+      self, accumulators: List[Dict[ColumnName, ColumnType]]
+  ) -> Dict[ColumnName, ColumnType]:
+    """Merge the feature types inferred from the different partitions.
+
+    Args:
+      accumulators: A list of dicts containing the feature types inferred from
+        the different partitions of the data.
+
+    Returns:
+      A dict containing the merged feature types.
+    """
+    result = {}
+    for shard_types in accumulators:
+      # Merge the types inferred in each partition using the type hierarchy.
+      # Specifically, whenever we observe a type higher in the type hierarchy
+      # we update the type.
+      for feature_name, feature_type in six.iteritems(shard_types):
+        if feature_name not in result or feature_type > result[feature_name]:
+          result[feature_name] = feature_type
+    return result
+
+  def extract_output(
+      self, accumulator: Dict[ColumnName, ColumnType]) -> List[ColumnInfo]:
+    """Return a list of tuples containing the column info."""
+    return [
+        ColumnInfo(col_name, accumulator.get(col_name, None))
+        for col_name in self._column_names
+    ]
+
+
+class _CSVRecordReader(object):
+  """A picklable wrapper for csv.reader that can decode one record at a time."""
+
+  __slots__ = ["_delimiter", "_line_iterator", "_reader", "_to_reader_input"]
+
+  def __init__(self, delimiter: Union[Text, bytes]):
+    self._delimiter = delimiter
+    self._line_iterator = _MutableRepeat()
+    self._reader = csv.reader(self._line_iterator, delimiter=delimiter)
+    # Python 2 csv reader accepts bytes while Python 3 csv reader accepts
+    # unicode.
+    if six.PY2:
+      self._to_reader_input = tf.compat.as_bytes
+    else:
+      self._to_reader_input = tf.compat.as_text
+
+  def read_line(self, csv_line: CSVLine) -> List[CSVCell]:
+    """Reads out bytes for PY2 and Unicode for PY3."""
+    self._line_iterator.set_item(self._to_reader_input(csv_line))
+    return [tf.compat.as_bytes(cell) for cell in next(self._reader)]
+
+  def __getstate__(self):
+    return (self._delimiter,)
+
+  def __setstate__(self, state):
+    self.__init__(*state)
+
+
+class _MutableRepeat(object):
+  """Similar to itertools.repeat, but the item can be set on the fly."""
+
+  def __init__(self):
+    self._item = None
+
+  def set_item(self, item: Any):
+    self._item = item
+
+  def __iter__(self) -> Any:
+    return self
+
+  def __next__(self) -> Any:
+    return self._item
+
+  next = __next__
+
+
+_INT64_MIN = np.iinfo(np.int64).min
+_INT64_MAX = np.iinfo(np.int64).max
+
+
+def _infer_value_type(value: CSVCell) -> ColumnType:
+  """Infer column type from the input value."""
+  # If the value is an empty string, we can set the type to be
+  # either FLOAT or STRING. We conservatively set it to be FLOAT.
+  if not value:
+    return ColumnType.FLOAT
+
+  # Check if the value is of type INT.
+  try:
+    if _INT64_MIN <= int(value) <= _INT64_MAX:
+      return ColumnType.INT
+    # We infer STRING type when we have long integer values.
+    return ColumnType.STRING
+  except ValueError:
+    # If the type is not INT, we next check for FLOAT type (according to our
+    # type hierarchy). If we can convert the string to a float value, we
+    # fix the type to be FLOAT. Else we resort to STRING type.
+    try:
+      float(value)
+    except ValueError:
+      return ColumnType.STRING
+    return ColumnType.FLOAT
---
+++ tfx_bsl/coders/csv_decoder_test.py
@@ -0,0 +1,259 @@
+# coding=utf-8
+#
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for CSV decoder."""
+
+from __future__ import absolute_import
+from __future__ import division
+# Standard __future__ imports
+from __future__ import print_function
+
+import apache_beam as beam
+from apache_beam.testing import util as beam_test_util
+import numpy as np
+from tfx_bsl.coders import csv_decoder
+from absl.testing import absltest
+from absl.testing import parameterized
+
+
+_TEST_CASES = [
+    dict(
+        testcase_name='simple',
+        input_lines=['1,2.0,hello', '5,12.34,world'],
+        column_names=['int_feature', 'float_feature', 'str_feature'],
+        expected_csv_cells=[
+            [b'1', b'2.0', b'hello'],
+            [b'5', b'12.34', b'world'],
+        ],
+        expected_types=[
+            csv_decoder.ColumnType.INT,
+            csv_decoder.ColumnType.FLOAT,
+            csv_decoder.ColumnType.STRING,
+        ]),
+    dict(
+        testcase_name='missing_values',
+        input_lines=['1,,hello', ',12.34,'],
+        column_names=['f1', 'f2', 'f3'],
+        expected_csv_cells=[
+            [b'1', b'', b'hello'],
+            [b'', b'12.34', b''],
+        ],
+        expected_types=[
+            csv_decoder.ColumnType.FLOAT,
+            csv_decoder.ColumnType.FLOAT,
+            csv_decoder.ColumnType.STRING,
+        ]),
+    dict(
+        testcase_name='mixed_int_and_float',
+        input_lines=['2,1.5', '1.5,2'],
+        column_names=['f1', 'f2'],
+        expected_csv_cells=[[b'2', b'1.5'], [b'1.5', b'2']],
+        expected_types=[
+            csv_decoder.ColumnType.FLOAT,
+            csv_decoder.ColumnType.FLOAT,
+        ],
+    ),
+    dict(
+        testcase_name='mixed_int_and_string',
+        input_lines=['2,abc', 'abc,2'],
+        column_names=['f1', 'f2'],
+        expected_csv_cells=[[b'2', b'abc'], [b'abc', b'2']],
+        expected_types=[
+            csv_decoder.ColumnType.STRING,
+            csv_decoder.ColumnType.STRING,
+        ],
+    ),
+    dict(
+        testcase_name='mixed_float_and_string',
+        input_lines=['2.3,abc', 'abc,2.3'],
+        column_names=['f1', 'f2'],
+        expected_csv_cells=[[b'2.3', b'abc'], [b'abc', b'2.3']],
+        expected_types=[
+            csv_decoder.ColumnType.STRING,
+            csv_decoder.ColumnType.STRING,
+        ],
+    ),
+    dict(
+        testcase_name='unicode',
+        input_lines=[u'\U0001f951'],
+        column_names=['f1'],
+        expected_csv_cells=[[u'\U0001f951'.encode('utf-8')]],
+        expected_types=[
+            csv_decoder.ColumnType.STRING,
+        ]),
+    dict(
+        testcase_name='quotes',
+        input_lines=['1,"ab,cd,ef"', '5,"wx,xy,yz"'],
+        column_names=['f1', 'f2'],
+        expected_csv_cells=[[b'1', b'ab,cd,ef'], [b'5', b'wx,xy,yz']],
+        expected_types=[
+            csv_decoder.ColumnType.INT,
+            csv_decoder.ColumnType.STRING,
+        ]),
+    dict(
+        testcase_name='space_delimiter',
+        input_lines=['1 "ab,cd,ef"', '5 "wx,xy,yz"'],
+        column_names=['f1', 'f2'],
+        expected_csv_cells=[[b'1', b'ab,cd,ef'], [b'5', b'wx,xy,yz']],
+        expected_types=[
+            csv_decoder.ColumnType.INT,
+            csv_decoder.ColumnType.STRING,
+        ],
+        delimiter=' '),
+    dict(
+        testcase_name='tab_delimiter',
+        input_lines=['1\t"this is a \ttext"', '5\t'],
+        column_names=['f1', 'f2'],
+        expected_csv_cells=[[b'1', b'this is a \ttext'], [b'5', b'']],
+        expected_types=[
+            csv_decoder.ColumnType.INT,
+            csv_decoder.ColumnType.STRING,
+        ],
+        delimiter='\t'),
+    dict(
+        testcase_name='negative_values',
+        input_lines=['-1,-2.5'],
+        column_names=['f1', 'f2'],
+        expected_csv_cells=[[b'-1', b'-2.5']],
+        expected_types=[
+            csv_decoder.ColumnType.INT,
+            csv_decoder.ColumnType.FLOAT,
+        ]),
+    dict(
+        testcase_name='int64_boundary',
+        input_lines=[
+            '%s,%s,%s,%s' % (
+                str(np.iinfo(np.int64).min),
+                str(np.iinfo(np.int64).max),
+                str(np.iinfo(np.int64).min - 1),
+                str(np.iinfo(np.int64).max + 1),
+            )
+        ],
+        column_names=['int64max', 'int64min', 'int64max+1', 'int64min-1'],
+        expected_csv_cells=[[
+            b'-9223372036854775808', b'9223372036854775807',
+            b'-9223372036854775809', b'9223372036854775808'
+        ]],
+        expected_types=[
+            csv_decoder.ColumnType.INT,
+            csv_decoder.ColumnType.INT,
+            csv_decoder.ColumnType.STRING,
+            csv_decoder.ColumnType.STRING,
+        ]),
+    dict(
+        testcase_name='skip_blank_lines',
+        input_lines=['', '1,2'],
+        skip_blank_lines=True,
+        column_names=['f1', 'f2'],
+        expected_csv_cells=[[], [b'1', b'2']],
+        expected_types=[
+            csv_decoder.ColumnType.INT,
+            csv_decoder.ColumnType.INT,
+        ],
+    ),
+    dict(
+        testcase_name='consider_blank_lines',
+        input_lines=['', '1,2'],
+        skip_blank_lines=False,
+        column_names=['f1', 'f2'],
+        expected_csv_cells=[[], [b'1', b'2']],
+        expected_types=[
+            csv_decoder.ColumnType.FLOAT,
+            csv_decoder.ColumnType.FLOAT,
+        ],
+    ),
+    dict(
+        testcase_name='skip_blank_lines_single_column',
+        input_lines=['', '1'],
+        skip_blank_lines=True,
+        column_names=['f1'],
+        expected_csv_cells=[[], [b'1']],
+        expected_types=[
+            csv_decoder.ColumnType.INT,
+        ],
+    ),
+    dict(
+        testcase_name='consider_blank_lines_single_column',
+        input_lines=['', '1'],
+        skip_blank_lines=False,
+        column_names=['f1'],
+        expected_csv_cells=[[], [b'1']],
+        expected_types=[
+            csv_decoder.ColumnType.FLOAT,
+        ],
+    ),
+    dict(
+        testcase_name='empty_csv',
+        input_lines=[],
+        column_names=['f1'],
+        expected_csv_cells=[],
+        expected_types=[None],
+    )
+]
+
+
+class CSVDecoderTest(parameterized.TestCase):
+  """Tests for CSV decoder."""
+
+  @parameterized.named_parameters(_TEST_CASES)
+  def test_parse_csv_lines(self,
+                           input_lines,
+                           column_names,
+                           expected_csv_cells,
+                           expected_types,
+                           skip_blank_lines=False,
+                           delimiter=','):
+
+    def _check_csv_cells(actual):
+      self.assertEqual(expected_csv_cells, actual)
+
+    def _check_types(actual):
+      self.assertLen(actual, 1)
+      self.assertCountEqual([
+          csv_decoder.ColumnInfo(n, t)
+          for n, t in zip(column_names, expected_types)
+      ], actual[0])
+
+    with beam.Pipeline() as p:
+      parsed_csv_cells = (
+          p | beam.Create(input_lines)
+          | beam.ParDo(csv_decoder.ParseCSVLine(delimiter=delimiter)))
+      inferred_types = parsed_csv_cells | beam.CombineGlobally(
+          csv_decoder.ColumnTypeInferrer(
+              column_names, skip_blank_lines=skip_blank_lines))
+
+      beam_test_util.assert_that(
+          parsed_csv_cells, _check_csv_cells, label='check_parsed_csv_cells')
+      beam_test_util.assert_that(
+          inferred_types, _check_types, label='check_types')
+
+  def test_invalid_row(self):
+    input_lines = ['1,2.0,hello', '5,12.34']
+    column_names = ['int_feature', 'float_feature', 'str_feature']
+    with self.assertRaisesRegexp(
+        ValueError, '.*Columns do not match specified csv headers.*'):
+      with beam.Pipeline() as p:
+        result = (
+            p | beam.Create(input_lines)
+            | beam.ParDo(csv_decoder.ParseCSVLine(delimiter=','))
+            | beam.CombineGlobally(
+                csv_decoder.ColumnTypeInferrer(
+                    column_names, skip_blank_lines=False)))
+        beam_test_util.assert_that(result, lambda _: None)
+
+
+if __name__ == '__main__':
+  absltest.main()
