433bd27854f2408d4552650e059644a45af4467f
zhuo <zhuo@google.com>
2020-02-19 11:01:12 -0800

    Created a test util for TFExample TFXIO.
    PiperOrigin-RevId: 296007957

---
+++ tfx_bsl/tfxio/test_util.py
@@ -0,0 +1,50 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Contains TFXIO helpers for testing purposes."""
+
+from __future__ import absolute_import
+from __future__ import division
+# Standard __future__ imports
+from __future__ import print_function
+
+import apache_beam as beam
+from tfx_bsl.tfxio import tf_example_record
+from tfx_bsl.tfxio import tfxio
+from typing import List, Text
+
+
+class InMemoryTFExampleRecord(tf_example_record._TFExampleRecordBase):  # pylint: disable=protected-access
+  """A TF Example TFXIO whose source is a PCollection[bytes].
+
+  Usage:
+    tfxio = InMemoryTFExampleRecord(schema)
+    record_batches = (
+      beam.Create([serialized_example1, ...]) | tfxio.BeamSource())
+  """
+
+  def _SerializedExamplesSource(self) -> beam.PTransform:
+
+    @beam.typehints.with_input_types(bytes)
+    @beam.typehints.with_output_types(bytes)
+    def _PTransformFn(raw_records_pcoll: beam.pvalue.PCollection):
+      return raw_records_pcoll
+
+    return beam.ptransform_fn(_PTransformFn)()
+
+  def TensorFlowDataset(self):
+    raise NotImplementedError
+
+  def _ProjectImpl(self, tensor_names: List[Text]) -> tfxio.TFXIO:
+    return InMemoryTFExampleRecord(
+        self._ProjectTfmdSchema(tensor_names), self.raw_record_column_name)
---
+++ tfx_bsl/tfxio/test_util_test.py
@@ -0,0 +1,59 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for tfx_bsl.tfxio.test_util."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import apache_beam as beam
+from apache_beam.testing import util as beam_testing_util
+import pyarrow as pa
+import tensorflow as tf
+from tfx_bsl.tfxio import test_util
+
+from google.protobuf import text_format
+from absl.testing import absltest
+
+
+class TestUtilTest(absltest.TestCase):
+
+  def testGetRecordBatches(self):
+    tfxio = test_util.InMemoryTFExampleRecord()
+    examples = [text_format.Parse("""
+    features {
+      feature {
+        key: "f1"
+        value {
+          int64_list {
+            value: 123
+          }
+        }
+      }
+    }""", tf.train.Example()).SerializeToString()]
+    def _AssertFn(record_batches):
+      self.assertLen(record_batches, 1)
+      record_batch = record_batches[0]
+      self.assertEqual(record_batch.num_rows, 1)
+      self.assertEqual(record_batch.num_columns, 1)
+      self.assertTrue(record_batch[0].equals(
+          pa.array([[123]], type=pa.list_(pa.int64()))))
+
+    with beam.Pipeline() as p:
+      record_batches = p | beam.Create(examples) | tfxio.BeamSource()
+      beam_testing_util.assert_that(record_batches, _AssertFn)
+
+
+if __name__ == "__main__":
+  absltest.main()
