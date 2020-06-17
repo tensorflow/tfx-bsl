24fe4585269f616732d2068194da760746b580e6
zhuo <zhuo@google.com>
2020-06-17 13:39:32 -0700

    Added a framework to convert Tensors/Composite Tensors to Arrow RecordBatches.
    PiperOrigin-RevId: 316955697

---
+++ tfx_bsl/tfxio/tensor_to_arrow.py
@@ -0,0 +1,229 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Utils to convert TF Tensors to Arrow arrays."""
+
+from __future__ import absolute_import
+from __future__ import division
+# Standard __future__ imports
+from __future__ import print_function
+
+import abc
+from typing import Dict, List, Text, Tuple, Union
+
+import pyarrow as pa
+import six
+import tensorflow as tf
+
+# CompositeTensor is not public yet.
+from tensorflow.python.framework import composite_tensor  # pylint: disable=g-direct-tensorflow-import
+from tensorflow_metadata.proto.v0 import schema_pb2
+
+
+TensorAlike = Union[tf.Tensor, composite_tensor.CompositeTensor]
+
+
+class TensorsToRecordBatchConverter(object):
+  """Converts a Dict[Text, TensorAlike] to a RecordBatch."""
+
+  __slots__ = ["_handlers", "_arrow_schema"]
+
+  def __init__(self, type_specs: Dict[Text, tf.TypeSpec]):
+    """Initializer.
+
+    Args:
+      type_specs: a mapping from names of tensors to their TypeSpecs. When
+        calling convert(), the dict of tensors passed in must contain the
+        same names, and each TensorAlike must be compatible to their TypeSpecs.
+    """
+    self._handlers = _make_handlers(type_specs)
+    all_fields = []
+    seen_column_names = set()
+    for tensor_name, handler in self._handlers:
+      for f in handler.arrow_fields():
+        if f.name in seen_column_names:
+          raise ValueError("Handler for tensor {} produces a column of a "
+                           "conflicting name: {}".format(tensor_name, f.name))
+        seen_column_names.add(f.name)
+        all_fields.append(f)
+    self._arrow_schema = pa.schema(all_fields)
+
+  def arrow_schema(self) -> pa.Schema:
+    """Returns the schema of the RecordBatch output by convert()."""
+    return self._arrow_schema
+
+  def tensor_representations(
+      self) -> Dict[Text, schema_pb2.TensorRepresentation]:
+    """Returns the TensorRepresentations for each TensorAlike.
+
+    The TypeSpecs of those TensorAlikes are specified in the initializer.
+    These TensorRepresentations, along with the schema returned by
+    arrow_schema() comprises all the information needed to turn the
+    RecordBatches produced by convert() back to TensorAlikes.
+
+    Returns:
+      a dict mapping tensor names to their TensorRepresentations.
+    """
+    return {
+        tensor_name: handler.tensor_representation()
+        for tensor_name, handler in self._handlers
+    }
+
+  def convert(self, tensors: Dict[Text, TensorAlike]) -> pa.RecordBatch:
+    """Converts a dict of tensors to a RecordBatch.
+
+    Args:
+      tensors: must contain the same keys as the dict passed to the initialier.
+        and each TensorAlike must be compatible with the corresponding TypeSpec.
+
+    Returns:
+      a RecordBatch, whose schema equals to self.arrow_schema().
+    """
+    assert len(self._handlers) == len(tensors)
+    arrays = []
+    for tensor_name, handler in self._handlers:
+      arrays.extend(handler.convert(tensors[tensor_name]))
+
+    return pa.record_batch(arrays, schema=self._arrow_schema)
+
+
+@six.add_metaclass(abc.ABCMeta)
+class _TypeHandler(object):
+  """Interface of a type handler that converts a tensor to arrow arrays.
+
+  Note that a handler may convert a Tensor to multiple pa.Arrays. See
+  arrow_fields().
+  """
+
+  __slots__ = ["_tensor_name", "_type_spec"]
+
+  def __init__(self, tensor_name: Text, type_spec: tf.TypeSpec):
+    self._tensor_name = tensor_name
+    self._type_spec = type_spec
+
+  def convert(self, tensor: TensorAlike) -> List[pa.Array]:
+    if not self._type_spec.is_compatible_with(tensor):
+      raise TypeError("Expected {} but got {}".format(
+          self._type_spec, _get_type_spec(tensor)))
+    return self._convert_internal(tensor)
+
+  @abc.abstractmethod
+  def arrow_fields(self) -> List[pa.Field]:
+    """Returns the name and type (in a pa.Field) of result pa.Arrays.
+
+    Note that a Handler can convert a Tensor to multiple pa.Arrays. It must
+    make sure _convert_internal() returns those Arrays of the types declared
+    here, in the correct order.
+    """
+
+  @abc.abstractmethod
+  def _convert_internal(self, tensor: TensorAlike) -> List[pa.Array]:
+    """Converts the given TensorAlike to a list of pa.Arrays.
+
+    Each element in the list should correspond to one in `arrow_fields()`.
+
+    Args:
+      tensor: the TensorAlike to be converted.
+    """
+
+  @abc.abstractmethod
+  def tensor_representation(self) -> schema_pb2.TensorRepresentation:
+    """Returns the TensorRepresentation.
+
+    The TensorRepresentation, along with `arrow_fields()` can be used to
+    convert from pa.Arrays back to Tensors.
+    """
+
+  @staticmethod
+  @abc.abstractmethod
+  def can_handle(type_spec: tf.TypeSpec) -> bool:
+    """Returns `True` if the handler can handle the given `tf.TypeSpec`."""
+
+
+class _VarLenSparseTensorHandler(_TypeHandler):
+  """Handles 2-D var-len (ragged) sparse tensor."""
+
+  __slots__ = ["_values_arrow_type"]
+
+  def __init__(self, tensor_name: Text, type_spec: tf.TypeSpec):
+    super(_VarLenSparseTensorHandler, self).__init__(tensor_name, type_spec)
+    self._values_arrow_type = _tf_dtype_to_arrow_type(type_spec.dtype)
+
+  def _convert_internal(self, tensor: TensorAlike) -> List[pa.Array]:
+    r = tf.RaggedTensor.from_sparse(tensor)
+    return [pa.ListArray.from_arrays(
+        pa.array(r.row_splits.numpy(), type=pa.int32()),
+        pa.array(r.values.numpy(), type=self._values_arrow_type))]
+
+  def arrow_fields(self) -> List[pa.Field]:
+    return [
+        pa.field(self._tensor_name,
+                 pa.list_(_tf_dtype_to_arrow_type(self._type_spec.dtype)))
+    ]
+
+  def tensor_representation(self) -> schema_pb2.TensorRepresentation:
+    result = schema_pb2.TensorRepresentation()
+    result.varlen_sparse_tensor.column_name = self._tensor_name
+    return result
+
+  @staticmethod
+  def can_handle(type_spec: tf.TypeSpec) -> bool:
+    if not isinstance(type_spec, tf.SparseTensorSpec):
+      return False
+    return (
+        type_spec.shape.is_compatible_with([None, None]) and
+        type_spec.dtype != tf.bool)
+
+
+_ALL_HANDLERS_CLS = [_VarLenSparseTensorHandler]
+
+
+def _tf_dtype_to_arrow_type(dtype: tf.DType):
+  """Maps a tf Dtype to an Arrow type."""
+  if dtype == tf.string:
+    return pa.binary()
+  elif dtype == tf.bool:
+    raise TypeError("Unable to handle bool tensors -- consider casting it to a "
+                    "tf.uint8")
+  return pa.from_numpy_dtype(dtype.as_numpy_dtype)
+
+
+def _get_type_spec(tensor_alike: TensorAlike):
+  """Returns the TypeSpec of a TensorAlike."""
+  if isinstance(tensor_alike, tf.Tensor):
+    return tf.TensorSpec.from_tensor(tensor_alike)
+  elif isinstance(tensor_alike, composite_tensor.CompositeTensor):
+    return tensor_alike._type_spec  # pylint:disable=protected-access
+  raise TypeError("Not a Tensor or CompositeTensor: {}".format(
+      type(tensor_alike)))
+
+
+def _make_handlers(
+    type_specs: Dict[Text, tf.TypeSpec]) -> List[Tuple[Text, _TypeHandler]]:
+  return [
+      (tensor_name, _get_handler(tensor_name, type_spec))
+      for tensor_name, type_spec in sorted(type_specs.items())
+  ]
+
+
+def _get_handler(
+    tensor_name: Text, type_spec: tf.TypeSpec) -> _TypeHandler:
+  """Returns a TypeHandler that can handle `type_spec`."""
+  for handler_cls in _ALL_HANDLERS_CLS:
+    if handler_cls.can_handle(type_spec):
+      return handler_cls(tensor_name, type_spec)
+  raise ValueError(
+      "No handler found for tensor {} of spec {}. "
+      "Note that tensors with dtype == tf.bool cannot be handled in general -- "
+      "consider casting them to tf.uint8."
+      .format(tensor_name, type_spec))
---
+++ tfx_bsl/tfxio/tensor_to_arrow_test.py
@@ -0,0 +1,187 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for tfx_bsl.tfxio.tensor_to_arrow."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import pyarrow as pa
+import tensorflow as tf
+from tfx_bsl.tfxio import tensor_adapter
+from tfx_bsl.tfxio import tensor_to_arrow
+from google.protobuf import text_format
+from absl.testing import absltest
+from absl.testing import parameterized
+from tensorflow_metadata.proto.v0 import schema_pb2
+
+
+_TF_TYPE_TO_ARROW_TYPE = {
+    tf.int8: pa.int8(),
+    tf.int16: pa.int16(),
+    tf.int32: pa.int32(),
+    tf.int64: pa.int64(),
+    tf.uint8: pa.uint8(),
+    tf.uint16: pa.uint16(),
+    tf.uint32: pa.uint32(),
+    tf.uint64: pa.uint64(),
+    tf.float32: pa.float32(),
+    tf.float64: pa.float64(),
+    tf.string: pa.binary(),
+}
+
+
+def _make_2d_varlen_sparse_tensor_test_cases():
+  result = []
+  for tf_type, arrow_type in _TF_TYPE_TO_ARROW_TYPE.items():
+    if tf_type == tf.string:
+      values = tf.constant([b"1", b"2", b"3"], dtype=tf.string)
+      expected_array = pa.array([[b"1"], [], [b"2", b"3"], []],
+                                type=pa.list_(arrow_type))
+    else:
+      values = tf.constant([1, 2, 3], dtype=tf_type)
+      expected_array = pa.array([[1], [], [2, 3], []],
+                                type=pa.list_(arrow_type))
+    result.append(dict(
+        testcase_name="2d_varlen_sparse_tensor_%s" % tf_type.name,
+        type_specs={"sp": tf.SparseTensorSpec([None, None], tf_type)},
+        expected_schema={
+            "sp": pa.list_(arrow_type)
+        },
+        expected_tensor_representations={
+            "sp": """varlen_sparse_tensor { column_name: "sp" }""",
+        },
+        tensor_input={
+            "sp":
+                tf.SparseTensor(
+                    values=values,
+                    indices=[[0, 0], [2, 0], [2, 1]],
+                    dense_shape=[4, 2]),
+        },
+        expected_record_batch={
+            "sp": expected_array
+        }
+        ))
+  return result
+
+_CONVERT_TEST_CASES = [
+    dict(
+        testcase_name="multiple_tensors",
+        type_specs={
+            "sp1": tf.SparseTensorSpec([None, None], tf.int32),
+            "sp2": tf.SparseTensorSpec([None, None], tf.string),
+        },
+        expected_schema={
+            "sp1": pa.list_(pa.int32()),
+            "sp2": pa.list_(pa.binary()),
+        },
+        expected_tensor_representations={
+            "sp1": """varlen_sparse_tensor { column_name: "sp1" }""",
+            "sp2": """varlen_sparse_tensor { column_name: "sp2" }""",
+        },
+        tensor_input={
+            "sp1":
+                tf.SparseTensor(
+                    values=tf.constant([1, 2], dtype=tf.int32),
+                    indices=[[0, 0], [2, 0]],
+                    dense_shape=[4, 1]),
+            "sp2":
+                tf.SparseTensor(
+                    values=[b"aa", b"bb"],
+                    indices=[[2, 0], [2, 1]],
+                    dense_shape=[4, 2])
+        },
+        expected_record_batch={
+            "sp1":
+                pa.array([[1], [], [2], []], type=pa.list_(pa.int32())),
+            "sp2":
+                pa.array([[], [], [b"aa", b"bb"], []],
+                         type=pa.list_(pa.binary()))
+        })
+] + _make_2d_varlen_sparse_tensor_test_cases()
+
+
+class TensorToArrowTest(tf.test.TestCase, parameterized.TestCase):
+
+  def _assert_tensor_alike_equal(self, left, right):
+    self.assertIsInstance(left, type(right))
+    if isinstance(left, tf.SparseTensor):
+      self.assertAllEqual(left.values, right.values)
+      self.assertAllEqual(left.indices, right.indices)
+      self.assertAllEqual(left.dense_shape, right.dense_shape)
+    else:
+      self.assertAllEqual(left, right)
+
+  @parameterized.named_parameters(*_CONVERT_TEST_CASES)
+  def test_convert(self, type_specs, expected_schema,
+                   expected_tensor_representations, tensor_input,
+                   expected_record_batch):
+    converter = tensor_to_arrow.TensorsToRecordBatchConverter(type_specs)
+
+    expected_schema = pa.schema(
+        [pa.field(n, t) for n, t in sorted(expected_schema.items())])
+
+    self.assertTrue(converter.arrow_schema().equals(expected_schema),
+                    "actual: {}".format(converter.arrow_schema()))
+
+    canonical_expected_tensor_representations = {}
+    for n, r in expected_tensor_representations.items():
+      if not isinstance(r, schema_pb2.TensorRepresentation):
+        r = text_format.Parse(r, schema_pb2.TensorRepresentation())
+      canonical_expected_tensor_representations[n] = r
+
+    self.assertEqual(canonical_expected_tensor_representations,
+                     converter.tensor_representations())
+
+    rb = converter.convert(tensor_input)
+    self.assertTrue(
+        rb.equals(
+            pa.record_batch(
+                [arr for _, arr in sorted(expected_record_batch.items())],
+                schema=expected_schema)))
+
+    # Test that TensorAdapter(TensorsToRecordBatchConverter()) is identity.
+    adapter = tensor_adapter.TensorAdapter(
+        tensor_adapter.TensorAdapterConfig(
+            arrow_schema=converter.arrow_schema(),
+            tensor_representations=converter.tensor_representations()))
+    adapter_output = adapter.ToBatchTensors(rb, produce_eager_tensors=True)
+    self.assertEqual(adapter_output.keys(), tensor_input.keys())
+    for k in adapter_output.keys():
+      self._assert_tensor_alike_equal(adapter_output[k], tensor_input[k])
+
+  def test_unable_to_handle(self):
+    with self.assertRaisesRegex(ValueError, "No handler found"):
+      tensor_to_arrow.TensorsToRecordBatchConverter(
+          {"sp": tf.SparseTensorSpec([None, None, None], tf.int32)})
+
+    with self.assertRaisesRegex(ValueError, "No handler found"):
+      tensor_to_arrow.TensorsToRecordBatchConverter(
+          {"sp": tf.SparseTensorSpec([None, None], tf.bool)})
+
+  def test_incompatible_type_spec(self):
+    converter = tensor_to_arrow.TensorsToRecordBatchConverter(
+        {"sp": tf.SparseTensorSpec([None, None], tf.int32)})
+    with self.assertRaisesRegex(TypeError, "Expected SparseTensorSpec"):
+      converter.convert({
+          "sp":
+              tf.SparseTensor(
+                  indices=[[0, 1]],
+                  values=tf.constant([0], dtype=tf.int64),
+                  dense_shape=[4, 1])
+      })
+
+
+if __name__ == "__main__":
+  absltest.main()
