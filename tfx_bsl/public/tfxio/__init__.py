efbd43c2381888b9741de4e30016ebb56f66ea96
zhuo <zhuo@google.com>
2020-07-30 11:07:10 -0700

    Introduced TFXIO public API symbols to tfx_bsl.public.tfxio
    PiperOrigin-RevId: 324040162

---
+++ tfx_bsl/public/tfxio/__init__.py
@@ -0,0 +1,32 @@
+# Copyright 2020 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Module level imports for tfx_bsl.public.tfxio.
+
+TFXIO defines a common in-memory data representation shared by all TFX libraries
+and components, as well as an I/O abstraction layer to produce such
+representations. See the RFC for details:
+https://github.com/tensorflow/community/blob/master/rfcs/20191017-tfx-standardized-inputs.md
+"""
+
+from tfx_bsl.tfxio.csv_tfxio import BeamRecordCsvTFXIO
+from tfx_bsl.tfxio.csv_tfxio import CsvTFXIO
+from tfx_bsl.tfxio.dataset_options import TensorflowDatasetOptions
+from tfx_bsl.tfxio.tensor_adapter import TensorAdapter
+from tfx_bsl.tfxio.tensor_adapter import TensorAdapterConfig
+from tfx_bsl.tfxio.tensor_adapter import TensorRepresentations
+from tfx_bsl.tfxio.tf_example_record import TFExampleBeamRecord
+from tfx_bsl.tfxio.tf_example_record import TFExampleRecord
+from tfx_bsl.tfxio.tf_sequence_example_record import TFSequenceExampleBeamRecord
+from tfx_bsl.tfxio.tf_sequence_example_record import TFSequenceExampleRecord
+from tfx_bsl.tfxio.tfxio import TFXIO
---
+++ tfx_bsl/public/tfxio/tfxio_import_test.py
@@ -0,0 +1,41 @@
+# Copyright 2020 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Test to make sure public TFXIO symbols can be imported."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from absl.testing import absltest
+
+
+class TfxioImportTest(absltest.TestCase):
+
+  def test_import(self):
+    # pylint: disable=g-import-not-at-top,unused-import
+    from tfx_bsl.public.tfxio import BeamRecordCsvTFXIO
+    from tfx_bsl.public.tfxio import CsvTFXIO
+    from tfx_bsl.public.tfxio import TensorflowDatasetOptions
+    from tfx_bsl.public.tfxio import TensorAdapter
+    from tfx_bsl.public.tfxio import TensorAdapterConfig
+    from tfx_bsl.public.tfxio import TensorRepresentations
+    from tfx_bsl.public.tfxio import TFExampleBeamRecord
+    from tfx_bsl.public.tfxio import TFExampleRecord
+    from tfx_bsl.public.tfxio import TFSequenceExampleBeamRecord
+    from tfx_bsl.public.tfxio import TFSequenceExampleRecord
+    from tfx_bsl.public.tfxio import TFXIO
+
+
+if __name__ == "__main__":
+  absltest.main()
