4540f550ec4201ceffbeab7369046e8b559d0500
tfx-bsl-team <tensorflow-extended-nonhuman@googlegroups.com>
2020-10-07 12:19:33 -0700

    Internal
    PiperOrigin-RevId: 335925199

--- tfx_bsl/tools/docker_build/build_manylinux.sh
+++ tfx_bsl/tools/docker_build/build_manylinux.sh
@@ -28,12 +28,10 @@ function setup_environment() {
   source scl_source enable devtoolset-8
   source scl_source enable rh-python36
   if [[ -z "${PYTHON_VERSION}" ]]; then
-    echo "Must set PYTHON_VERSION env to 35|36|37|38"; exit 1;
+    echo "Must set PYTHON_VERSION env to 36|37|38"; exit 1;
   fi
   # Bazel will use PYTHON_BIN_PATH to determine the right python library.
-  if [[ "${PYTHON_VERSION}" == 35 ]]; then
-    PYTHON_DIR=/opt/python/cp35-cp35m
-  elif [[ "${PYTHON_VERSION}" == 36 ]]; then
+  if [[ "${PYTHON_VERSION}" == 36 ]]; then
     PYTHON_DIR=/opt/python/cp36-cp36m
   elif [[ "${PYTHON_VERSION}" == 37 ]]; then
     PYTHON_DIR=/opt/python/cp37-cp37m
