6182a5eec0a04168d3a6112a16b4c92d126870f2
tfx-bsl-team <tensorflow-extended-nonhuman@googlegroups.com>
2023-11-13 17:01:12 -0800

    Change rh-python39 to rh-python38 and pip3 to pip in Docker builds.
    PiperOrigin-RevId: 582128724

--- tfx_bsl/tools/docker_build/build_manylinux.sh
+++ tfx_bsl/tools/docker_build/build_manylinux.sh
@@ -26,7 +26,7 @@ WORKING_DIR=$PWD
 
 function setup_environment() {
   source scl_source enable devtoolset-8
-  source scl_source enable rh-python39
+  source scl_source enable rh-python38
   if [[ -z "${PYTHON_VERSION}" ]]; then
     echo "Must set PYTHON_VERSION env to 39|310|311"; exit 1;
   fi
@@ -47,7 +47,7 @@ function setup_environment() {
   ${PIP_BIN} install --upgrade pip
   ${PIP_BIN} install wheel --upgrade
   ${PIP_BIN} install "numpy~=1.22.0" --force
-  pip3 install auditwheel
+  ${PIP_BIN} install auditwheel
 }
 
 function build_wheel() {
