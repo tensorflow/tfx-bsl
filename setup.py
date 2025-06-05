078a12ede29d2859220584eb2825d28e9064c9d4
tfx-bsl-team <tensorflow-extended-nonhuman@googlegroups.com>
2025-06-05 16:06:18 -0700

    Update 1.17.1 version.py and RELEASE.md

--- setup.py
+++ setup.py
@@ -170,7 +170,7 @@ setup(
     install_requires=[
         'absl-py>=0.9,<2.0.0',
         'apache-beam[gcp]>=2.53,<3;python_version>="3.11"',
-        'apache-beam[gcp]>=2.47,<3;python_version<"3.11"',
+        'apache-beam[gcp]>=2.50,<2.51;python_version<"3.11"',
         'google-api-python-client>=1.7.11,<2',
         'numpy>=1.22.0',
         'pandas>=1.0,<2',
