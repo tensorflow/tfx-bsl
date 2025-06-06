9fedd4eacedfd1835e3eb4d81e3bf9a6d569a707
andrewfulton9 <andrewfulton9@gmail.com>
2025-06-06 11:51:36 -0600

    update ferror-limit

--- setup.py
+++ setup.py
@@ -90,7 +90,8 @@ class _BazelBuildCommand(setuptools.Command):
                 self._additional_build_options = [
                     "--macos_minimum_os=11.0",
                     "--config=macos_arm64",
-                    "--verbose_failures"
+                    "--verbose_failures",
+                    "-ferror-limit=0"
 
                 ]
             else:
