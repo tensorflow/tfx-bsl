0939b5568555785fd3f7ab853ed1bedabdb3f381
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2025-07-29 19:42:06 -0700

    Use apache-beam[gcp]>=2.66,<3 for all python versions

--- setup.py
+++ setup.py
@@ -171,8 +171,8 @@ setup(
     # and protobuf) with TF.
     install_requires=[
         "absl-py>=0.9,<2.0.0",
-        'apache-beam[gcp]>=2.53,<3;python_version>="3.11"',
-        'apache-beam[gcp]>=2.50,<2.51;python_version<"3.11"',
+        "apache-beam[gcp]>=2.66,<3",
+        # 'apache-beam[gcp]>=2.50,<2.51;python_version<"3.11"',
         "google-api-python-client>=1.7.11,<2",
         "numpy",
         "pandas>=1.0,<2",
